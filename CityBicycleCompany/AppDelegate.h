027394b4892212d566b316433c768915f67db649
Valak12 <Kohnechi@gmail.com>
2014-11-26 13:42:44 -0800

    Initial Commit

---
+++ CityBicycleCompany/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityBicycleCompany
+//
+//  Created by Vala Kohnechi on 11/26/14.
+//  Copyright (c) 2014 MVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityBicycleCompany/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CityBicycleCompany
+//
+//  Created by Vala Kohnechi on 11/26/14.
+//  Copyright (c) 2014 MVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
