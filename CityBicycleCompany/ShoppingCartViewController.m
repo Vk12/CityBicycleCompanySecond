4b302ce47de60b76238114ee367601cd643155d6
May Yang <mayyang@Mays-MacBook-Pro.local>
2014-11-28 15:54:07 -0800

    set up intial query from example

--- CityBicycleCompany/ShoppingCartViewController.m
+++ CityBicycleCompany/ShoppingCartViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ShoppingCartViewController.h"
-//#import "Stripe+ApplePay.h"
-//#import "Stripe.h"
+#import "Stripe+ApplePay.h"
+#import "Stripe.h"
 @interface ShoppingCartViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *buyWithIpayButton;
 
@@ -24,22 +24,22 @@
 
 - (IBAction)onPayButtonTapped:(UIButton *)sender
 {
-//    PKPaymentRequest *request = [Stripe
-//                                 paymentRequestWithMerchantIdentifier:YOUR_APPLE_MERCHANT_ID];
-//    // Configure your request here.
-//    NSString *label = @"Premium Llama Food";
-//    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:@"10.00"];
-//    request.paymentSummaryItems = @[
-//                                    [PKPaymentSummaryItem summaryItemWithLabel:label
-//                                                                        amount:amount];
-//                                    ];
-//    
-//    if ([Stripe canSubmitPaymentRequest:request]) {
-//        ...
-//    } else {
-//        // Show the user your own credit card form (see options 2 or 3)
-//    }
-
+    PKPaymentRequest *request = [Stripe paymentRequestWithMerchantIdentifier:@"merchant.com.citybicyclecompany"];
+    
+    // Configure your request here.
+    NSString *label = @"Premium llama food";
+    NSDecimalNumber *number = [NSDecimalNumber decimalNumberWithString:@"10.00"];
+    request.paymentSummaryItems = @[[PKPaymentSummaryItem summaryItemWithLabel:label amount:number]];
+    
+    if ([Stripe canSubmitPaymentRequest:request]) {
+        // Do stuff.
+    }
+    else
+    {
+        // Show the user your own credit card form (see options 2 or 3).
+    }
+    
+    
 }
 
 
