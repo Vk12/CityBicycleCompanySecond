6a8fd9b8d9d53f85e936d77e5bc0e37c05d60c85
Valak12 <Kohnechi@gmail.com>
2014-12-01 09:27:07 -0800

    Added cell

---
+++ CityBicycleCompany/ProductCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ProductCollectionViewCell.m
+//  CityBicycleCompany
+//
+//  Created by Vala Kohnechi on 12/1/14.
+//  Copyright (c) 2014 MVA. All rights reserved.
+//
+
+#import "ProductCollectionViewCell.h"
+
+@implementation ProductCollectionViewCell
+
+@end
--- CityBicycleCompany/ProductViewController.m
+++ CityBicycleCompany/ProductViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ProductViewController.h"
+#import "ProductCollectionViewCell.h"
 
 @interface ProductViewController ()<UICollectionViewDataSource,UICollectionViewDelegate>
 @property (strong, nonatomic) IBOutlet UICollectionView *productCollectionView;
