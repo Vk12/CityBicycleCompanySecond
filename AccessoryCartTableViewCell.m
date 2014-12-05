6e332c144c5c5afea1b7b1481fb520067fdec12c
Archer <archer@oodalalee.com>
2014-12-04 19:52:10 -0800

    updated constraints

---
+++ AccessoryCartTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  AccessoryCartTableViewCell.m
+//  CityBicycleCompany
+//
+//  Created by Supreme Overlord on 12/4/14.
+//  Copyright (c) 2014 MVA. All rights reserved.
+//
+
+#import "AccessoryCartTableViewCell.h"
+
+@implementation AccessoryCartTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
