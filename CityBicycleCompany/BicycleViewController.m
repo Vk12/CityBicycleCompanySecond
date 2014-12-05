77767cd90aaf49921383c382394f12526991c223
Valak12 <Kohnechi@gmail.com>
2014-12-05 11:04:35 -0800

    Fixed some things with bicycle

--- CityBicycleCompany/BicycleViewController.m
+++ CityBicycleCompany/BicycleViewController.m
@@ -74,8 +74,32 @@
     
     
 }
+- (IBAction)onCartButtonTapped:(UIButton *)sender
+{
+//    self shouldPerformSegueWithIdentifier:@"bicycleToCartSegue" sender:sender
+//    if (<#condition#>) {
+//        <#statements#>
+//    }
+}
 
-
+-(BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender
+{
+    if (self.sizeSegmentedController.selectedSegmentIndex >= 0 && self.wheelSetColorSegmented.selectedSegmentIndex >= 0 && self.classicSeriesWheelsetSegmented.selectedSegmentIndex >= 0 && self.rearBreakController.selectedSegmentIndex >= 0)
+    {
+        [self performSegueWithIdentifier:@"bicycleToCartSegue" sender:sender];
+        return YES;
+    } else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"City Bicycle Company"
+                                                        message:@"Please select all items before proceding"
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return NO;
+        
+    }
+}
 
 - (IBAction)dismissOnTapped:(UIButton *)sender
 {
@@ -187,6 +211,8 @@
     }
     
     self.localChosenBike.chosenWheelSetColor = self.bicycleFromParse.wheelsetColor[self.wheelSetColorSegmented.selectedSegmentIndex];
+    
+   
     
     [self.addToCartArray addObject:self.localChosenBike];
     
