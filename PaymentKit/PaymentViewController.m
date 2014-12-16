9b6729bb069881514e543722845415aef1263bd9
May <mayy.yang@gmail.com>
2014-12-15 22:47:17 -0800

    committing before checkout

--- PaymentKit/PaymentViewController.m
+++ PaymentKit/PaymentViewController.m
@@ -17,24 +17,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    self.paymentView = [[PTKView alloc] initWithFrame:CGRectMake(15, 25, 290, 55)];
-//    self.paymentView.delegate = self;
-//    [self.view addSubview:self.paymentView];
+    self.paymentView = [[PTKView alloc] initWithFrame:CGRectMake(15, 25, 290, 55)];
+    self.paymentView.delegate = self;
+    [self.view addSubview:self.paymentView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) paymentView:(PTKView *)paymentView withCard:(PTKCard *)card isValid:(BOOL)valid
+{
+    NSLog(@"Card number: %@", card.number);
+    NSLog(@"Card expiry: %lu/%lu", (unsigned long)card.expMonth, (unsigned long)card.expYear);
+    NSLog(@"Card cvc: %@", card.cvc);
+    NSLog(@"Address zip: %@", card.addressZip);
+    
+     self.navigationItem.rightBarButtonItem.enabled = valid;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
